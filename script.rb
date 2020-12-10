bfb2b5d360b5b47f6f51c8a88a12706146465f8b
Alejandro Contreras <kdta15@outlook.com>
2020-12-10 12:38:15 -0600

    Added the function my_any? to the Enumerables module

--- script.rb
+++ script.rb
@@ -52,6 +52,20 @@ module Enumerable
     end
     true
   end
+
+  def my_any?
+    if block_given?
+      i = 0
+      length.times do
+        value = yield self[i]
+        return true if value == true
+        i += 1
+      end
+    else
+      to_enum(:my_any)
+    end
+    false
+  end
 end
 
 
@@ -62,9 +76,11 @@ a = [1,2,3,4,5,6]
 #a.my_each {|n| puts n + 5 + 2}
 #a.my_each_with_index {|n, i| print n + 5, i}
 #print a.my_select {|n| n > 2}
-puts a.my_all? {|n| n > 9}
+#puts a.my_all? {|n| n > 9}
+#puts a.my_any? {|n| n < 9}
 
 #a.each {|n| puts n + 5 + 2}
 #a.each_with_index {|n, i| print n + 5, i}
 #print a.select {|n| n > 2}
-puts a.all? {|n| n > 9}
+#puts a.all? {|n| n > 9}
+#puts a.any? {|n| n < 9}
