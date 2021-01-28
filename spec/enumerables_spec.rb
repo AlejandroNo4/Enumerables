482bb40cdc21667d012f50f1ee938f99c40e0e0d
Alejandro Contreras <kdta15@outlook.com>
2021-01-28 14:38:15 -0600

    Added last examples of bad usage

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -17,8 +17,8 @@ end
 
 describe Enumerable do
   describe '.my_each' do
-    let(:test_one) {[1, 2, 3].my_each { |a| a += 1 }}
-    it 'tests the BAD USE of .my_each, this will returns the same array' do
+    let(:test_one) { [1, 2, 3].my_each { |a| a + 1 } }
+    it 'tests the BAD USAGE of .my_each, this will return the same array' do
       expect(test_one).not_to eql([2, 3, 4])
     end
   end
@@ -41,8 +41,8 @@ end
 
 describe Enumerable do
   describe '.my_each_with_index' do
-    let(:test_one) {[1, 2, 3].my_each_with_index { |a, i| a += i }}
-    it 'tests the BAD USE of .my_each_with_index, this will NOT return the sum from test_one' do
+    let(:test_one) { [1, 2, 3].my_each_with_index { |a, i| a + i } }
+    it 'tests the BAD USAGE of .my_each_with_index, this will NOT return the sum from test_one' do
       expect(test_one).not_to eql([1, 3, 5])
     end
   end
@@ -64,9 +64,9 @@ end
 
 describe Enumerable do
   describe '.my_select' do
-    it 'tests the BAD USE of .my_select, the method ALWAYS will return a new array' do
+    it 'tests the BAD USAGE of .my_select, the method ALWAYS will return a new array' do
       res = [1, 2, 3, 4, 5]
-      res.my_select { |a| a > 2}
+      res.my_select { |a| a > 2 }
       expect(res).not_to eql([3, 4, 5])
     end
   end
@@ -101,11 +101,20 @@ describe Enumerable do
 end
 
 describe Enumerable do
+  describe '.my_all?' do
+    it 'tests the BAD USAGE of .my_all. With hashes, each key/value is passed as two-valued array' do
+      ages = { 'bob' => 22, 'Jay' => 32 }
+      expect(ages.my_all?(Numeric)).not_to eql(true)
+    end
+  end
+end
+
+describe Enumerable do
   describe 'my_any?' do
     let(:test_one) { %w[ant bear cat].my_any? { |word| word.length >= 3 } }
     let(:test_two) { %w[ant bear cat].my_any? { |word| word.length >= 4 } }
     let(:test_three) { [1, 2i, 3.14].my_any?(Integer) }
-    let(:test_four) { [].my_any? }
+    let(:test_four) { [nil, true, 99].my_any? }
     context 'returns true if the block ever returns a value OTHER THAN false or nil' do
       it 'test one to true' do
         expect(test_one).to eql(true)
@@ -121,9 +130,18 @@ describe Enumerable do
         expect(test_three).to eql(true)
       end
 
-      it 'test example three to be false' do
-        expect(test_four).to eql(false)
+      it 'without a block or parameter, it will always be true unless one of the values is nil or false' do
+        expect(test_four).to eql(true)
       end
+    end
+  end
+end
+
+describe Enumerable do
+  describe '.my_any?' do
+    it 'tests the BAD USAGE of .my_all. With hashes. Empty array or hash, will always be false' do
+      a = []
+      expect(a.my_any?).not_to eql(true)
     end
   end
 end
@@ -158,6 +176,15 @@ describe Enumerable do
 end
 
 describe Enumerable do
+  describe '.my_none?' do
+    it 'tests the BAD USAGE of .my_none. With hashes, this will return true unless the poarameter Object' do
+      ages = { 'bob' => 22, 'Jay' => 32 }
+      expect(ages.my_none?(Numeric)).not_to eql(false)
+    end
+  end
+end
+
+describe Enumerable do
   describe 'my_count' do
     let(:arr_test) { [1, 2, 4, 2] }
     let(:arr_with_block) { arr_test.my_count(&:even?) }
@@ -179,6 +206,15 @@ describe Enumerable do
 end
 
 describe Enumerable do
+  describe '.my_count' do
+    it 'tests the BAD USAGE of .my_none. A parameter biger than the length, will return 0' do
+      ary = [1, 2, 4, 2]
+      expect(ary.count(5)).not_to eql(5)
+    end
+  end
+end
+
+describe Enumerable do
   describe 'my_map' do
     it 'returns a new array with the results of running block once for every element in the array' do
       res = [1, 2, 3, 4].my_map { |i| i * i }
@@ -188,6 +224,16 @@ describe Enumerable do
     it 'returns Enumerable if no block is given' do
       no_block = [1, 2, 3, 4].my_map
       expect(no_block.is_a?(Enumerable)).to eql(true)
+    end
+  end
+end
+
+describe Enumerable do
+  describe '.my_map' do
+    it 'tests the BAD USAGE of .my_map. the method ALWAYS will return a new array' do
+      res = [1, 2, 3]
+      res.my_map { |a| a * 2 }
+      expect(res).not_to eql([2, 4, 6])
     end
   end
 end
@@ -207,6 +253,16 @@ describe Enumerable do
 
     it 'uses a symbol that names the operator and specify an initial value' do
       expect(test_three).to eql(151_200)
+    end
+  end
+end
+
+describe Enumerable do
+  describe '.my_inject' do
+    it 'tests the BAD USAGE of .my_map. when passed over each key-value, it converts it first into an array' do
+      hashy = { 'one' => 1, 'two' => 2, 'three' => 3 }
+      res = hashy.inject { |sum, n| sum + n }
+      expect(res).not_to eql(6)
     end
   end
 end
